3a98cf6ed3fb2714910e8967cb087640e4999d16
onnsy <orawee.hinthong@gmail.com>
2023-07-17 22:18:46 +0700

    add director page

---
+++ pages/directors.vue
@@ -0,0 +1,173 @@
+<template>
+    <sidenav ref="sidenav" @submenuWidth="getWidth"/>
+    <div class="main" :style="`padding-left: ${width}px;`">
+      <Header />
+      <div class="w-100">
+        <section class="bg-primary d-lg-flex banner hp-banner">
+          <img src="/banner_fn.png" class="main-logo" />
+          <div class="col">
+            <img src="/director-banner.jpg" class="img-fluid w-100" />
+          </div>
+          <div class="col py-5 py-lg-0 d-flex align-items-center justify-content-center">
+            <h2 class="text-white font-normal mb-0">BOARD OF<br>DIRECTORS</h2>
+          </div>
+        </section>
+        <section class="py-5">
+          <div class="container">
+            <h2 class="heading-text text-center">
+              <img src="/logo.png" class="img-fluid mb-4" />
+              <span>BOARD OF DIRECTORS</span>
+            </h2>
+            <div class="row">
+              <div class="col-lg-4 mt-5">
+                <div class="bod">
+                  <div class="img">
+                    <img src="/director-1.jpg" class="img-fluid"/>
+                  </div>
+                  <h3>MR.PRICHA SONGWATANA</h3>
+                  <p>Chairman of the Board of Directors</p>
+                </div>
+              </div>
+              <div class="col-lg-4 mt-5">
+                <div class="bod">
+                  <div class="img">
+                    <img src="/director-2.jpg" class="img-fluid"/>
+                  </div>
+                  <h3>MRS.SAOWANEE KAMOLBUTR</h3>
+                  <p>The 1st Vice Chairman of the Board of Directors
+/ Independent Director</p>
+                </div>
+              </div>
+              <div class="col-lg-4 mt-5">
+                <div class="bod">
+                  <div class="img">
+                    <img src="/director-3.png" class="img-fluid"/>
+                  </div>
+                  <h3>MR.MONTRI MAHAPLERKPONG</h3>
+                  <p>Director</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+      </div>
+      <Footer></Footer>
+    </div>
+  </template>
+  
+  <script>
+  import { defineComponent } from '@vue/composition-api'
+  
+  export default defineComponent({
+    data() {
+      return {
+        width: '50',
+      };
+    },
+    mounted() {
+      console.log( this.width )
+    },
+    methods: {
+      getWidth( submenuWidth) {
+        this.width = submenuWidth
+      },
+    },
+  })
+  </script>
+  
+<style lang="scss" scoped>
+.hp-banner{
+  position: relative;
+  .main-logo{
+    position: absolute;
+    width: 150px;
+    left: 50%;
+    top: 50%;
+    margin-left: 7px;
+    transform: translate(-50%,-50%);
+  }
+}
+
+.p-15{
+  padding: 75px;
+}
+
+  .border-bottom-red{
+    border-bottom: 2px solid #CD3832;
+  }
+
+  .text-img{
+    height: 25px;
+    width: auto;
+  }
+
+  .malls{
+    justify-content: center;
+    a{
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      background: #CC3832;
+      width: 150px;
+      height: 150px;
+      border-radius: 50%;
+      @media (max-width: 992px) {
+        margin: 15px auto !important;
+      }
+    }
+  }
+
+  
+  .main{
+    padding-left: 100px;
+  }
+
+  .heading-text{
+    @media (max-width: 992px) {
+      flex-wrap: wrap;
+      img{
+        height: 30px;
+      }
+      span{
+        font-size: 30px;
+        @media (max-width: 992px) {
+          display: block;
+          line-height: 1;
+          //font-size: 22px;
+        }
+      }
+    }
+  }
+
+  label{
+    color: #fff;
+    margin-top: 15px;
+  }
+
+  .form-control{
+    border-radius: 0;
+  }
+
+  .btn-white{
+    border-radius: 0;
+  }
+
+  .bod{
+    text-align: center;
+    border-bottom: 2px solid #CC3832;
+    height: 100%;
+    .img{
+      background: #CC3832;
+    }
+    h3{
+      margin-top: 15px;
+      color: #555754;
+      font-weight: normal;
+    }
+    p{
+      color: #555754;
+    }
+  }
+
+  </style>
+  
