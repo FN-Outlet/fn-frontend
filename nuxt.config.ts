a04d6f7397218e20e59bc58781e7b24d5be87fb0
Supakorn Kimhajan <supakorn@Supakorns-MacBook-Pro.local>
2023-07-27 08:07:09 +0700

    update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,6 +5,18 @@ export default defineNuxtConfig({
     '@nuxtjs/i18n',
   ],
   i18n: {
+    detectBrowserLanguage: {
+      // If enabled, a cookie is set once a user has been redirected to his
+      // preferred language to prevent subsequent redirections
+      // Set to false to redirect every time
+      useCookie: true,
+      // Cookie name
+      cookieKey: 'i18n_redirected',
+      // Set to always redirect to value stored in the cookie, not just once
+      alwaysRedirect: false,
+      // If no locale for the browsers locale is a match, use this one as a fallback
+      fallbackLocale: 'th'
+    },
     langDir: './locales/',
     locales: [
       { code: 'en', iso: 'en-US', file: 'en.json' },
