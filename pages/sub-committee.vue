c80af3462c65e0d2a3d96424def5c2128500c2a4
onnsy <orawee.hinthong@gmail.com>
2023-08-01 22:29:24 +0700

    sample

--- pages/sub-committee.vue
+++ pages/sub-committee.vue
@@ -29,6 +29,75 @@
               </div>
             </div>
             <div class="row" >
+              <div class="modal-overlay-custom" v-if="showModal" @click="showModal=false">
+                <div class="modal-custom" @click.stop>
+                  <div class="img">
+                    <img src="/director-2.jpg" class="img-fluid" />
+                  </div>
+                  <article>
+                    <header class="heading">
+                      <h3 class="line">Mrs.Saowanee Kamolbutr</h3>
+                      <p>Chairman of the Audit Committee</p>
+                    </header>
+                    <div class="description">
+                      <table class="toc-profile">
+                        <tbody>
+                          <tr>
+                            <th>Qualification</th>
+                            <td>
+                              <ul class="type-squre">
+                                <li>Master of Political Science (Public Administration), Thammasat University</li>
+                                <li>Bachelor of Political Science (Public Administration), Thammasat University</li>
+                              </ul>
+                            </td>
+                          </tr>
+                          <tr>
+                            <th>Training</th>
+                            <td>
+                              <ul class="type-squre">
+                                <li>Certification Program by Thai Institute Directors Association (IOD), IT Governance and Cyber Resilience Program (ITG), Class 15/2020</li>
+                                <li>Certification Program by Thai Institute Directors Association (IOD), Boards that make a difference (BMD), Class 9/2019</li>
+                                <li>Certification Program by Thai Institute Directors Association (IOD), Advance Audit Committee Program (AACP), Class 17/2017</li>
+                                <li>Certification Program by Thai Institute Directors Association (IOD), Financial Institutions Governance Program (FGP), Class 1/2010</li>
+                                <li>Certification Program by Thai Institute Directors Association (IOD), Role of the Chairman Program (RCP), Class 23/2010</li>
+                                <li>Certification Program by Thai Institute Directors Association (IOD), Role of the Compensation Committee (RCC), Class 8/2009</li>
+                                <li>Certification Program by Thai Institute Directors Association (IOD), Director Certification Program (DCP), Class 69/2006</li>
+                                <li>Senior Executive Program (Kellogg-Sasin)</li>
+                                <li>The Management Development Program (Wharton School) Certificate in Public Sector Reform, University of Victoria, New Zealand</li>
+                                <li>National Defense College of Thailand (NDC), Class of 2005</li>
+                                <li>Capital Market Academy (CMA), Class 7</li>
+                                <li>Advanced Security Management Program Alumni (ASMA.), Class 4 </li>
+                              </ul>
+                            </td>
+                          </tr>
+                          <tr>
+                            <th>Work Experiences</th>
+                            <td>
+                              <ul class="type-squre">
+                              <li>2021 - present Independent Director / Member of Audit Committee Thitikorn Public Company Limited </li>
+                              <li>2021 - present Chairman of Board of Directors AIRA Asset Management (AIAM) </li>
+                              <li>2020 - present Honorary Director Southeast Bangkok College</li>
+                              <li>2020 - present Independent Director / Chairman of the Audit Committee Global Power Synergy Pcl. </li>
+                              <li>2017 - present Independent Director / Chairman of the Audit Committee / Chairman of the Risk Management Committee /  Member of the Nomination and Remuneration Committee, Carabao Group Pcl.</li>
+                              <li>2017 - present Independent Director / Chairman of the Audit Committee / Risk Management Committee T.K.S. Technologies Pcl.</li>
+                              <li>2017 - 2019 Chairman  / Independent Director Pacific Pipe Pc</li>
+                              <li>2016 - 2020 Independent Director / Chairman of the Audit Committee /  Member of the Nomination and Remuneration Committee,  Global Power Synergy Pcl.</li>
+                              <li>2016 - 2017 Financial Expert Port Authority of Thailand</li>
+                              <li>2016 - 2019 Advisor, The Committee on Economics, Monetary Affairs and Finance The Committee of the National Legislative Assembly</li>
+                              <li>2016 - 2018 Director / Qualified person in the Council of Ministers Rajapark Institute</li>
+                              <li>2016 - 2017 Director SME Development Bank</li>
+                              <li>2016 - 2017 Independent Director /  Member of the Audit Committee Interlink Company Communication Pcl.</li>
+                                  </ul></td>
+                          </tr>
+                        </tbody>
+                      </table>
+                    </div>
+                  </article>
+                </div>
+                <div class="close" @click="showModal=false">
+                  <img class="close-img img-fluid" src="/close.svg" alt="" />
+                </div>
+              </div>
               <div class="col-lg-4 mt-5" v-for="(item, index) in data" :key="index">
                 <div class="bod">
                   <!--<div class="img" @click="showModal = true">-->
@@ -39,15 +108,7 @@
                       @clicked="closeModal(false)"
                     >
                     </info-modal>-->
-                    <div class="modal-overlay-custom" v-if="showModal==true"  @click="closeModal">
-                    <div class="modal-custom"  @click.stop>
-                      <h6>Saved!</h6>
-                      <button>Go Home</button>
-                    </div>
-                    <div class="close" @click="closeModal">
-                      <img class="close-img img-fluid" src="/close.svg" alt="" />
-                    </div>
-                  </div>
+                    
                   </div>
                   <div v-if="$i18n.locale === 'en'">
                     <h3>{{ item.attributes.nameen }}</h3>
@@ -256,7 +317,7 @@
   }
 
 
-  .modal-overlay-custom {
+ .modal-overlay-custom {
   position: fixed;
   top: 0;
   bottom: 0;
@@ -264,18 +325,18 @@
   right: 0;
   display: flex;
   justify-content: center;
-  background:rgba(0,0,0,0.2);
-  z-index: 9;
+  background:rgba(0,0,0,0.5);
+  z-index: 99;
 }
 
 .modal-custom {
   text-align: center;
   background-color: white;
-  height: 500px;
-  width: 500px;
+  height: 80vh;
+  max-width: 80%;
   margin-top: 10%;
-  padding: 60px 0;
   border-radius: 20px;
+  overflow: hidden;
 }
 .close {
   margin: 10% 0 0 16px;
@@ -286,5 +347,30 @@
   width: 25px;
 }
 
+.modal-custom{
+  display: flex;
+  > *{
+    flex: 50%;
+  }
+  article{
+    text-align: left;
+    padding: 30px;
+    .description{
+      overflow: auto;
+      border-top: 1px solid #000000;
+      height: calc( 80vh - 150px );
+    }
+    table{
+      font-size: 12px;
+      td{
+        border-left: 1px solid #999;
+      }
+      th{
+        padding-right: 15px;
+      }
+    }
+    
+  }
+}
   </style>
   
