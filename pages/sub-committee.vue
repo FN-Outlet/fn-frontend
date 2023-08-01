40438cf0117e734c34653a06e6cf9173f25421f3
onnsy <orawee.hinthong@gmail.com>
2023-08-01 21:53:43 +0700

    modal

--- pages/sub-committee.vue
+++ pages/sub-committee.vue
@@ -32,13 +32,22 @@
               <div class="col-lg-4 mt-5" v-for="(item, index) in data" :key="index">
                 <div class="bod">
                   <!--<div class="img" @click="showModal = true">-->
-                  <div class="img" >
+                  <div class="img" @click="showModal = true">
                     <img v-if="item.attributes.image.data" :src="item.attributes.image.data.attributes.url" class="img-fluid" />
-                    <info-modal 
+                    <!--<info-modal 
                       v-if="showModal" 
                       @clicked="closeModal(false)"
                     >
-                    </info-modal>
+                    </info-modal>-->
+                    <div class="modal-overlay-custom" v-if="showModal==true"  @click="closeModal">
+                    <div class="modal-custom"  @click.stop>
+                      <h6>Saved!</h6>
+                      <button>Go Home</button>
+                    </div>
+                    <div class="close" @click="closeModal">
+                      <img class="close-img img-fluid" src="/close.svg" alt="" />
+                    </div>
+                  </div>
                   </div>
                   <div v-if="$i18n.locale === 'en'">
                     <h3>{{ item.attributes.nameen }}</h3>
@@ -83,8 +92,7 @@
       getWidth( submenuWidth) {
         this.width = submenuWidth
       },
-      closeModal( value ){
-        console.log( value )
+      closeModal(){
         this.showModal = false
         console.log( this.showModal )
       },
@@ -246,6 +254,37 @@
       }
     }
   }
+
+
+  .modal-overlay-custom {
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  display: flex;
+  justify-content: center;
+  background:rgba(0,0,0,0.2);
+  z-index: 9;
+}
+
+.modal-custom {
+  text-align: center;
+  background-color: white;
+  height: 500px;
+  width: 500px;
+  margin-top: 10%;
+  padding: 60px 0;
+  border-radius: 20px;
+}
+.close {
+  margin: 10% 0 0 16px;
+  cursor: pointer;
+}
+
+.close-img {
+  width: 25px;
+}
 
   </style>
   
